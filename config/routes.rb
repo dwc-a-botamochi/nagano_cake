235bccee3c6b1df3a1fb3216e36a55215cad4fed
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-04-18 14:09:38 +0000

    nagano_cake_feature-nabe_25/04/18_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+ 
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -6,10 +8,23 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
+scope module: "public" do
+  get   "/customers/my_page", to: "customers#show"
+  get   "/customers/information/edit", to: "customers#edit"
+  patch "/customers/information", to: "customers#update"
+  get   "/customers/check", to: "customers#check"
+  patch "/customers/withdraw", to: "customers#withdraw"
+end
+
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  resources :customers, only: [:index, :show, :edit, :update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
