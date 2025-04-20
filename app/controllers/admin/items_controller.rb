2e582027f14e82c22fc666e2d02f84b2591a0818
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-04-20 05:17:16 +0000

    item（仮)1

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,13 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    #@genres = Genre.all
+  end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect to admin_item_path(@item)
   end
 
   def show
@@ -13,5 +19,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
+  end
+
 end
