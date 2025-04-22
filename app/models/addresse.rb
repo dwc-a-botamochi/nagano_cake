ae115374c8b152c48c9efa1638e40d5b51d4de2b
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-04-22 13:32:38 +0000

    nagano_cake_feature-nabe_25/04/22_1

--- app/models/addresse.rb
+++ app/models/addresse.rb
@@ -1,5 +1,6 @@
 class Addresse < ApplicationRecord
-  # belongs_to :customer
+  belongs_to :customer
 
-  validates :post_code, :address, :name, presence: true        # 空でない
+  validates :post_code, :address, :name, presence: true         # 空でない
+  validates :post_code, format: { with: /\A\d{7}\z/ }           # 郵便番号（ハイフンなし7桁）
 end
