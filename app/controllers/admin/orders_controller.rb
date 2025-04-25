2419780bf2823339fea34a10a7e614b15e4b851e
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-04-25 03:47:04 +0000

    nagano_cake_feature-nabe_add_bootstrap_order_etc_25/04/25_1

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,11 +8,12 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
+
     if @order.update(order_params)
       if @order.payment_checked?
-        @order.order_details.update_all(making_status: :making_pending)
+        @order.order_details.update_all(making_status: OrderDetail.making_statuses[:making_pending])
       end
-    end    
+    end
     redirect_to admin_order_path(@order)
   end
 
